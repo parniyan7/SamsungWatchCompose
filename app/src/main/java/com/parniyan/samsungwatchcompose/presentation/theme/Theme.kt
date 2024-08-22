e23d7d56a1fe7926ebdbf521e54b8f926fbd3fa0
parniyan <mousaieparniyan@gmail.com>
2024-08-22 21:15:38 +0330

    initial commit

---
+++ app/src/main/java/com/parniyan/samsungwatchcompose/presentation/theme/Theme.kt
@@ -0,0 +1,17 @@
+package com.parniyan.samsungwatchcompose.presentation.theme
+
+import androidx.compose.runtime.Composable
+import androidx.wear.compose.material.MaterialTheme
+
+@Composable
+fun SamsungWatchComposeTheme(
+    content: @Composable () -> Unit
+) {
+    /**
+     * Empty theme to customize for your app.
+     * See: https://developer.android.com/jetpack/compose/designsystems/custom
+     */
+    MaterialTheme(
+        content = content
+    )
+}
